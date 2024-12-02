6953533406948f4a2245c5a4a961525f3077bb4b
Ana Nóbrega <ana.nobrega09@aluno.ifce.edu.br>
2024-12-02 08:43:55 -0300

    Update Principal.java

--- CAP06/EXERCICIOS-RESOLVIDOS/EXE04/src/br/edu/principal/Principal.java
+++ CAP06/EXERCICIOS-RESOLVIDOS/EXE04/src/br/edu/principal/Principal.java
@@ -4,36 +4,46 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		double N1, N2, N3;
-		
-		System.out.println("Insira o primeiro número: ");
-		N1 = sc.nextDouble();
-		System.out.println("Insira o segundo número: ");
-		N2 = sc.nextDouble();
-		System.out.println("Insira o terceiro número: ");
-		N3 = sc.nextDouble();
-		
-		if (N1<N2 & N1<N3 & N2<N3) {
-			System.out.println("a ordem crescente vai ser: "+N1+" - "+N2+" - "+N3);
-		}
-		else if(N1<N2 & N1<N3 & N3<N2) {
-			System.out.println("a ordem crescente vai ser: "+N1+" - "+N3+" - "+N2);
+		int[] numero = new int[8];
+		int[] posicao = new int[8];
+		int[] neg = new int[8];
+		int cont_N = 1, cont_P = 1;
+		for(int i = 1 ; i < 8; i++ ) {
+			System.out.println("Insira o valor desse número: ");
+			numero[i] = sc.nextInt();
+			if(numero[i]>= 0) {
+				posicao[cont_P] = numero[i];
+				cont_P +=1;
+				
 			}
-		else if(N2<N1 & N2<N3 & N1<N3) {
-			System.out.println("a ordem crescente vai ser: "+N2+" - "+N1+" - "+N3);
+			else {
+				neg[cont_N] = numero[i];
+				cont_N +=1;				
+			}
+			
 		}
-		else if(N2<N1 & N2<N3 & N3<N1) {
-			System.out.println("a ordem crescente vai ser: "+N2+" - "+N3+" - "+N1);
-		}
-		else if(N3<N1 & N3<N2 & N1<N2) {
-			System.out.println("a ordem crescente vai ser: "+N3+" - "+N1+" - "+N2);
+		if(cont_N == 1) {
+			System.out.println("vetores negativados e vazios: ");
+			
 		}
 		else {
-			System.out.println("a ordem crescente vai ser: "+N3+" - "+N2+" - "+N1);
+			for(int i = 1; i < cont_N - 1; i++) {
+				System.out.println("Vetor de negativos que está vazio");
+				System.out.println(neg[i]);	
+			}
 		}
-
+		if(cont_P == 1) {
+			System.out.println("Vetor de positivos que está vazio");
+		}
+		else {
+			for(int i =1; i <cont_P; i++) {
+				System.out.println(posicao[i]);
+			
+			}
+		}
 
 	}
 
 }
+
 
